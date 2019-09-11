3987101292635c959507f9c327ee581abf0cd55c
kolkatajubi <kolkatachamps@jubi.ai>
2019-09-11 20:50:12 +0530

    msg

--- Pixie/test.js
+++ Pixie/test.js
@@ -1,11 +1,12 @@
 var db = require("./db");
 (async () => {
-  let resp = await db.createUser({
-    username: "Sebastian",
-    password: "1234",
-    referral: "q45@hX"
-  });
+  //   let resp = await db.createUser({
+  //     username: "Sebastian",
+  //     password: "1234",
+  //     referral: "q45@hX"
+  //   });
+  let resp = await db.readByReferral("q45@hX");
   console.log(JSON.stringify(resp));
-  // await db.readByReferral
-  // await db.readByUsername
+  let resp = await db.readByUsername("Sebastian");
+  console.log(JSON.stringify(resp));
 })();
